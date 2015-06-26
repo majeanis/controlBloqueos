30e8e837b46bd51a83962995dfcbf5531d591cf5
Mauricio Cámara <mcamara75@yahoo.es>
2015-06-26 23:23:06 +0000

    Se incluye creación de Token
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@15 9a6f62dd-0dde-484e-8008-d119f4366752

--- base-datos/functions/init_bd.sql
+++ base-datos/functions/init_bd.sql
@@ -3,6 +3,7 @@ FUNCTION init_bd( i_FechaInit timestamp )
   RETURNS void AS $$
 DECLARE
     l_EstacionId    rcb_estc.estc_id%type;
+    l_UbicacionId   rcb_ubic.ubic_id%type;
 BEGIN
 
     delete from rcb_ubic;
@@ -17,8 +18,10 @@ BEGIN
               ,i_FechaInit )
     ;
 
+    select nextval( 'SEQ_ID' ) into l_UbicacionId;
+
     insert into rcb_ubic(ubic_id, ubic_nomb, ubic_desc, ubic_vige, estc_id, audi_fech_crea)
-       values ( nextval( 'SEQ_ID' )
+       values ( l_UbicacionId
               ,'CHANCADOR SECUNDARIO PLANTA 2'
               ,'CHANCADOR SECUNDARIO PLANTA 2'
               ,true
@@ -26,5 +29,9 @@ BEGIN
               ,i_FechaInit )
     ;
 
+    insert into rcb_tokn (tokn_id, ubic_id, audi_fech_crea)
+        values( uuid_generate_v1()
+              , l_UbicacionId
+              , localtimestamp );
 END;
 $$ LANGUAGE plpgsql;
