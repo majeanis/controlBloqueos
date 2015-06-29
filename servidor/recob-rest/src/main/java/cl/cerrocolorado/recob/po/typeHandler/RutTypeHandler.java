b5e3193d9660e14a4085ce55c39d5e64e61959f5
Mauricio Cámara <mcamara75@yahoo.es>
2015-06-29 20:38:40 +0000

    Corrección problema TypeHandler del tipo Rut
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@41 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/po/typeHandler/RutTypeHandler.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/po/typeHandler/RutTypeHandler.java
@@ -1,19 +1,22 @@
-package cl.cerrocolorado.recob.utils;
+package cl.cerrocolorado.recob.po.typeHandler;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.MappedTypes;
 
+import cl.cerrocolorado.recob.utils.Rut;
+
 /**
  * @author mauricio.camara
  */
-@MappedJdbcTypes(value={JdbcType.VARCHAR})
-@MappedTypes(value={Rut.class})
+@MappedJdbcTypes(JdbcType.VARCHAR)
+@MappedTypes(Rut.class)
 public class RutTypeHandler extends BaseTypeHandler<Rut>
 {
     @Override
@@ -25,18 +28,27 @@ public class RutTypeHandler extends BaseTypeHandler<Rut>
     @Override
     public Rut getNullableResult(ResultSet rs, String columnName) throws SQLException
     {
+        String rut = rs.getString(columnName);
+        if(rut == null ) return null;
+
         return Rut.valueOf( rs.getString(columnName) );
     }
 
     @Override
     public Rut getNullableResult(ResultSet rs, int i) throws SQLException
     {
+        String rut = rs.getString(i);
+        if(rut == null ) return null;
+
         return Rut.valueOf( rs.getString(i) );
     }
 
     @Override
     public Rut getNullableResult(CallableStatement cs, int i) throws SQLException
     {
+        String rut = cs.getString(i);
+        if(rut == null ) return null;
+
         return Rut.valueOf( cs.getString(i) );
     }
 }
