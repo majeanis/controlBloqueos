55c64343efc873a8675bd7e70a1352a6df2705f4
Mauricio Cámara <mcamara75@yahoo.es>
2015-06-29 16:13:12 +0000

    Se mueve clase de Test a otro directorio
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@38 9a6f62dd-0dde-484e-8008-d119f4366752

---
+++ servidor/recob-rest/src/test/java/cl/cerrocolorado/recob/test/TestBO.java
@@ -0,0 +1,46 @@
+package cl.cerrocolorado.recob.test;
+
+import cl.cerrocolorado.recob.bo.FactoryBO;
+import cl.cerrocolorado.recob.to.EmpresaTO;
+import cl.cerrocolorado.recob.utils.Rut;
+
+/**
+ *
+ * @author mauricio.camara
+ */
+public class TestBO
+{
+    public static void testEmpresa()
+    {
+        EmpresaTO empr = new EmpresaTO();
+
+        empr.setRut( Rut.valueOf("13005188-K") );
+        empr.setNombre("Mauricio Camara Molina");
+        empr.setVigente(Boolean.TRUE);
+        
+        FactoryBO.getEmpresaBO().guardar(empr);
+        System.out.println( empr );
+        System.out.println( FactoryBO.getEmpresaBO().getTodos() );
+    }
+
+    public static void main( String[] args ) 
+    {
+        testEmpresa();
+//        
+//        CajaBloqueoTO caja = new CajaBloqueoTO();
+//        CajaBloqueoBO bo = FactoryBO.getCajaBloqueoBO();
+//
+//        caja.setNumero(2);
+//        caja.setNombre("CAJA N° " + caja.getNumero() );
+//        caja.setUbicacion( new UbicacionTO() );
+//        caja.setVigente(Boolean.TRUE);
+//        caja.getUbicacion().setId(2);
+//        caja.getUbicacion().setEstacion(new EstacionTO() );
+//        caja.getUbicacion().getEstacion().setId(1);
+//        
+//        bo.guardar(caja);
+//        
+//        List<CajaBloqueoTO> cajas = bo.getVigentes(caja.getUbicacion());
+//        System.out.println("retorno:" + cajas);
+    }
+}
