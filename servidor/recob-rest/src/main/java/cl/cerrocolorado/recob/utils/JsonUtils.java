c5204e702e7c67469e524370a96a13a0a6e55db7
Mauricio Cámara <mcamara75@yahoo.es>
2015-07-11 19:59:11 +0000

    Corrección formato para el tipo Date
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@52 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/utils/JsonUtils.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/utils/JsonUtils.java
@@ -1,5 +1,7 @@
 package cl.cerrocolorado.recob.utils;
 
+import java.text.SimpleDateFormat;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -11,8 +13,9 @@ public class JsonUtils
 	static
 	{
 		mapper = new ObjectMapper();
-		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
 	}
 
 	public static String toJsonString(Object obj)
