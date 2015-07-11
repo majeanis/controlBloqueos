acdf1bc93207e13cc7d5052af6ce2cc212b0503a
Mauricio Cámara <mcamara75@yahoo.es>
2015-07-11 19:59:49 +0000

    Se corrigen dependencias
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@53 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/rest/ConfiguracionService.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/rest/ConfiguracionService.java
@@ -1,6 +1,5 @@
 package cl.cerrocolorado.recob.rest;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -11,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
