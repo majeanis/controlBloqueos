cd446725d7ea6311d1b034d2e83df8a74e5dae91
Mauricio Cámara <mcamara75@yahoo.es>
2015-08-17 04:18:35 +0000

    Se cambia "data" por "datos"
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@146 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/bo/BaseBO.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/bo/BaseBO.java
@@ -11,10 +11,10 @@ import cl.cerrocolorado.recob.utils.Transaccional;
 public interface BaseBO<T extends BaseTO> 
 {
 	@Transaccional
-    public Respuesta<T> crear(T data) throws Exception;
+    public Respuesta<T> crear(T datos) throws Exception;
 
  	@Transaccional
-    public Respuesta<T> modificar(T data) throws Exception;
+    public Respuesta<T> modificar(T datos) throws Exception;
 
 	@Transaccional
     public Respuesta<T> eliminar(T pk) throws Exception;
