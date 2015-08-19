297532cd11f028de6b23e3767f885cb6230aebf6
Mauricio Cámara <mcamara75@yahoo.es>
2015-08-19 03:59:48 +0000

    Se implementan métodos "crear" y "modificar" basados en "guardar"
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@152 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/bo/beans/EquipoBean.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/bo/beans/EquipoBean.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import cl.cerrocolorado.recob.bo.EquipoBO;
-import cl.cerrocolorado.recob.bo.utils.Transaccion;
 import cl.cerrocolorado.recob.po.EquipoPO;
 import cl.cerrocolorado.recob.to.EquipoTO;
 import cl.cerrocolorado.recob.to.EquipoTagsTO;
@@ -36,7 +35,7 @@ public class EquipoBean implements EquipoBO
     @Autowired
     private EquipoPO equipoPO;
 
-    public Respuesta<EquipoTO> guardar(EquipoTO equipo, boolean esNuevo)
+    private Respuesta<EquipoTO> guardar(EquipoTO equipo, boolean esNuevo)
     {
         logger.info("guardar[INI] equipo: {}", equipo);
         logger.info("guardar[INI] esNuevo: {}", esNuevo);
@@ -113,123 +112,6 @@ public class EquipoBean implements EquipoBO
         return guardar(datos, false);
     }
     
-    public Respuesta<EquipoTagsTO> guardar(EquipoTagsTO equipo, boolean esNuevo) throws Exception
-    {
-        logger.info("guardar[INI] equipo: {}", equipo);
-
-        // Se utilizará control manual de transacciones, ya que internamente
-        // llamaremos a un método que también será transaccional y eventualmente
-        // necesitaremos hacer rollback, cuando haya fallas de validación
-        Transaccion txLocal = new Transaccion();
-
-        try
-        {
-            txLocal.begin();
-           
-            Respuesta<EquipoTO> re = guardar((EquipoTO) equipo);
-            if( !re.getResultado().esExitoso() )
-            {
-                txLocal.rollback();
-                logger.info("guardar[FIN] errores al guardar equipo: {}", re);
-                return Respuesta.of(re.getResultado());
-            }
-            
-            logger.debug("guardar[001] después de guardar el equipo: {}", equipo);
-
-            Resultado rtdo = new ResultadoProceso();
-            List<TagTO> tags = equipo.getTags();
-            for(int i=0; i < tags.size(); i++ )
-            {
-                TagTO tag = tags.get(i);
-                tag.setEquipo(equipo);
-
-                Respuesta<TagTO> rt = guardarTag(tag);
-                if(!rt.getResultado().esExitoso())
-                {
-                    logger.debug("guardar[003] errores al validar el TAG: {}", tag);
-                    rtdo.append(rt.getResultado(), " [indice:" + (i + 1) + "]");
-                }
-            }
-            
-            if(rtdo.esExitoso())
-            {
-                rtdo.addMensaje(this.getClass(), "Equipo guardado con éxito");
-                txLocal.commit();
-                logger.info("guardar[FIN] después del commit: {}", equipo);
-                return Respuesta.of(rtdo, equipo);
-            } else
-            {
-                txLocal.rollback();
-                logger.info("guardar[FIN] después del rollback: {} {}", rtdo, equipo);
-                return Respuesta.of(rtdo);
-            }
-        } catch(Exception e)
-        {
-            txLocal.rollback();
-            logger.error("guardar[ERR] exception ", e);
-            throw e;
-        }
-    }
-
-    private Respuesta<TagTO> guardarTag(TagTO tag) throws Exception
-    {
-    	logger.info("guardarTag[INI] tag: {}", tag);
-    	
-    	Resultado rtdo = new ResultadoProceso();
-    	if( tag == null )
-    	{
-    		rtdo.addError(this.getClass(), "Debe informar los datos del TAG");
-    		logger.info("guardarTag[FIN] el tag llegó en NULL");
-    		return Respuesta.of(rtdo);
-    	}
-    	
-    	if(tag.getEnergiaCero() == null)
-    	{
-    		rtdo.addError(this.getClass(), "Debe informar la energía cero del TAG" );
-    	}
-    	if(tag.getVigente() == null)
-    	{
-    		rtdo.addError(this.getClass(), "Debe informar la vigencia del TAG" );
-    	}
-    	if(StringUtils.isBlank(tag.getDescripcion()))
-    	{
-    		rtdo.addError(this.getClass(), "Debe informar la descripción del TAG" );
-		}
-    	if(tag.getEquipo()==null || tag.getEquipo().isKeyBlank())
-    	{
-    		rtdo.addError(this.getClass(), "El TAG debe estar asociado a un equipo" );
-    	}
-    	if(StringUtils.isBlank(tag.getNombre()))
-    	{
-    		rtdo.addError(this.getClass(), "Debe informar el nombre del TAG" );
-    	}
-    	if(tag.getNumero() == null)
-    	{
-    		rtdo.addError(this.getClass(),  "Debe informar el N° del TAG" );
-    	}
-    	
-    	if(!rtdo.esExitoso())
-    	{
-    		logger.info("guardarTag[FIN] hubo errores de validación: {}", rtdo);
-    		return Respuesta.of(rtdo);
-    	}
-    	
-    	TagTO otro = equipoPO.getTag(tag);
-    	logger.debug("guardarTag[001] despues de buscar al TAG: {}", otro);
-
-    	if( otro != null )
-    	{
-    		tag.setId(otro.getId());
-    	}
-
-    	equipoPO.guardarTag(tag);
-
-        rtdo.addMensaje(this.getClass(), "TAG guardado con éxito");
-        
-    	logger.info("guardarTag[FIN] tag guardado con éxito: {}", tag);
-    	return Respuesta.of(rtdo, tag);
-    }
-    
     @Transaccional
     @Override
     public Respuesta<EquipoTO> eliminar(EquipoTO pk) throws Exception
@@ -258,7 +140,7 @@ public class EquipoBean implements EquipoBO
         {
             if( !equipoPO.esTagEliminable(tag) )
             {
-                rtdo.addError(this.getClass(), "TAG N° #{1} tiene registros asociados", String.valueOf(tag.getNumero()));
+                rtdo.addError(this.getClass(), "TAG #{1} tiene registros asociados", String.valueOf(tag.getCodigo()));
             }
         }
 
@@ -266,15 +148,6 @@ public class EquipoBean implements EquipoBO
         {
             logger.info("eliminar[FIN] se encontraron errores de validación");
             return Respuesta.of(rtdo);
-        }
-
-        // Revisamos si es posible eliminar los TAGs
-        for( TagTO tag: equipo.getTags())
-        {
-            if( !equipoPO.esTagEliminable(tag) )
-            {
-                rtdo.addError(this.getClass(), "TAG N° #{1} tiene registros asociados", String.valueOf(tag.getNumero()));
-            }
         }
         
         equipoPO.eliminarTags(equipo);
@@ -284,6 +157,86 @@ public class EquipoBean implements EquipoBO
 
         logger.info ("eliminar[FIN] registro eliminado con éxito: {}", equipo);
         return Respuesta.of(rtdo,equipo);
+    }
+    
+    private Respuesta<TagTO> guardarTag(TagTO tag, boolean esNuevo)
+    {
+    	logger.info("guardarTag[INI] tag: {}", tag);
+    	logger.info("guardarTag[INI] esNuevo: {}", esNuevo);
+        
+    	Resultado rtdo = new ResultadoProceso();
+    	if( tag == null )
+    	{
+    		rtdo.addError(this.getClass(), "Debe informar los datos del TAG");
+    		logger.info("guardarTag[FIN] el tag llegó en NULL");
+    		return Respuesta.of(rtdo);
+    	}
+    	
+    	if(tag.getEnergiaCero() == null)
+    	{
+    		rtdo.addError(this.getClass(), "Debe informar la energía cero del TAG" );
+    	}
+    	if(tag.getVigente() == null)
+    	{
+    		rtdo.addError(this.getClass(), "Debe informar la vigencia del TAG" );
+    	}
+    	if(StringUtils.isBlank(tag.getDescripcion()))
+    	{
+    		rtdo.addError(this.getClass(), "Debe informar la descripción del TAG" );
+		}
+    	if(tag.getEquipo()==null || tag.getEquipo().isKeyBlank())
+    	{
+    		rtdo.addError(this.getClass(), "El TAG debe estar asociado a un equipo" );
+    	}
+    	if(StringUtils.isBlank(tag.getCodigo()))
+    	{
+    		rtdo.addError(this.getClass(),  "Debe informar el código del TAG" );
+    	}
+        
+        TagTO otro = equipoPO.getTag(tag);
+    	if(esNuevo)
+        {
+            if( otro != null )
+            {
+                rtdo.addError(this.getClass(), "Ya existe TAG con código #{1}", tag.getCodigo());
+            }
+        } 
+        else if( otro == null )
+        {
+            rtdo.addError(this.getClass(), "No existe TAG con código #{1}", tag.getCodigo());
+        } 
+        else
+        {
+            tag.setId(otro.getId());
+        }
+
+    	if(!rtdo.esExitoso())
+    	{
+    		logger.info("guardarTag[FIN] hubo errores de validación: {}", rtdo);
+    		return Respuesta.of(rtdo);
+    	}
+
+    	equipoPO.guardarTag(tag);
+        logger.debug("guardar[001] después de guardar el TAG: {}", tag);
+
+        rtdo.addMensaje(this.getClass(), "TAG guardado con éxito");
+        
+    	logger.info("guardarTag[FIN] tag guardado con éxito: {}", tag);
+    	return Respuesta.of(rtdo, tag);
+    }
+
+    @Override
+    @Transaccional
+    public Respuesta<TagTO> crearTag(TagTO tag) throws Exception
+    {
+        return guardarTag(tag,true);
+    }
+
+    @Override
+    @Transaccional
+    public Respuesta<TagTO> modificarTag(TagTO tag) throws Exception
+    {
+        return guardarTag(tag,false);
     }
 
     @Transaccional
@@ -303,7 +256,7 @@ public class EquipoBean implements EquipoBO
         TagTO tag = equipoPO.getTag(pk);
         if( tag == null)
         {
-            rtdo.addError(this.getClass(), "No existe TAG con N° #{1}", String.valueOf(pk.getNumero()) );
+            rtdo.addError(this.getClass(), "No existe TAG con N° #{1}", String.valueOf(pk.getCodigo()) );
             logger.info("eliminarTag[FIN] no existe el TAG: {}", pk);
             return Respuesta.of(rtdo);
         }
@@ -318,8 +271,16 @@ public class EquipoBean implements EquipoBO
         equipoPO.eliminarTag(tag);
         rtdo.addMensaje(this.getClass(), "Registro eliminado con éxito");
         
-        logger.info ("eliminarTag[FIN] registro elimiinado con éxito: {}", tag);        
+        logger.info ("eliminarTag[FIN] registro eliminado con éxito: {}", tag);        
         return Respuesta.of(rtdo,tag);
+    }
+    
+    @Override
+    public Respuesta<EquipoTO> get(EquipoTO pk)
+    {
+        Respuesta<EquipoTagsTO> r = getEquipo( pk );
+        EquipoTO equipo = r.getContenido().orElse(null);
+        return Respuesta.of(r.getResultado(), equipo);
     }
 
     @Override
@@ -385,7 +346,7 @@ public class EquipoBean implements EquipoBO
         TagTO tag = equipoPO.getTag(pk);
         if( tag == null )
         {
-            rtdo.addError(this.getClass(), "No existe TAG N° #{1}", String.valueOf(pk.getNumero()));
+            rtdo.addError(this.getClass(), "No existe TAG #{1}", String.valueOf(pk.getCodigo()));
         }
         
         logger.info ("getTag[FIN] tag retornado: {}", tag);
@@ -413,43 +374,23 @@ public class EquipoBean implements EquipoBO
     }
 
     @Override
-    public Respuesta<List<TagTO>> getTags(EquipoTO pk, 
-                                          Optional<Boolean> vigencia)
+    public Respuesta<List<TagTO>> getTags(EquipoTO pk, Optional<Boolean> vigencia)
     {
-        logger.info("getTagsTodos[INI] pk: {}", pk);
-        logger.info("getTagsTodos[INI] vigencia: {}", vigencia);
+        logger.info("getTags[INI] pk: {}", pk);
+        logger.info("getTags[INI] vigencia: {}", vigencia);
         
         Resultado rtdo = new ResultadoProceso();
         if(pk==null || pk.isKeyBlank() )
         {
             rtdo.addError(this.getClass(), "Debe informar la ubicación" );
-            logger.info("getTagsTodos[FIN] no se informó bien la pk del equipo: {}", pk);
+            logger.info("getTags[FIN] no se informó bien la pk del equipo: {}", pk);
             return Respuesta.of(rtdo);
         }
         
         List<TagTO> lista = equipoPO.getTags(pk, Optional.empty(), vigencia);
         rtdo.addMensaje(new RegistrosQueryInfo(this.getClass(), lista.size()));
 
-        logger.info("getTagsTodos[FIN] registros retornados: {}", lista.size());
+        logger.info("getTags[FIN] registros retornados: {}", lista.size());
         return Respuesta.of(rtdo, lista);
-    }
-
-    @Override
-    @Transaccional
-    public Respuesta<TagTO> crearTag(TagTO tag) throws Exception
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Respuesta<TagTO> modificarTag(TagTO tag) throws Exception
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Respuesta<EquipoTO> get(EquipoTO pk)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
