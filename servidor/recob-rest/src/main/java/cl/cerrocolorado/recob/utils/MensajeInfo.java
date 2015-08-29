cbb7cbdabf8abc43b12657fb881db49bae99c945
Mauricio Cámara <mcamara75@yahoo.es>
2015-08-29 03:20:55 +0000

    Avance al día 29.08.2015
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@156 9a6f62dd-0dde-484e-8008-d119f4366752

---
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/utils/MensajeInfo.java
@@ -0,0 +1,21 @@
+package cl.cerrocolorado.recob.utils;
+
+public class MensajeInfo extends Mensaje
+{
+    private static final long serialVersionUID = 1L;
+
+    public MensajeInfo(Class<?> clazz, String textoBase, Object...valores)
+    {
+        super(Severidad.OK, clazz.getSimpleName(), textoBase, valores );
+    }
+
+    public MensajeInfo(Class<?> clazz, Exception exception)
+    {
+        super(clazz.getSimpleName(), exception);
+    }
+
+    public MensajeInfo(Mensaje another)
+    {
+        super(Severidad.OK, another.getCodigo(), another.getTexto() );
+    }
+}
