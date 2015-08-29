53d8e892b7a5fcc252b1958e576d3b60fcfdaf1c
Mauricio Cámara <mcamara75@yahoo.es>
2015-08-29 19:12:05 +0000

    Se agregan métodos factory
    git-svn-id: file:///drives/mega/svn-repos/controlBloqueos/branches/dev1.0@161 9a6f62dd-0dde-484e-8008-d119f4366752

--- servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/rest/utils/RespRest.java
+++ servidor/recob-rest/src/main/java/cl/cerrocolorado/recob/rest/utils/RespRest.java
@@ -1,6 +1,9 @@
 package cl.cerrocolorado.recob.rest.utils;
 
+import cl.cerrocolorado.recob.utils.Mensaje;
+import cl.cerrocolorado.recob.utils.Respuesta;
 import cl.cerrocolorado.recob.utils.Resultado;
+import cl.cerrocolorado.recob.utils.ResultadoProceso;
 import cl.cerrocolorado.recob.utils.ToStringUtils;
 
 public class RespRest<T> 
@@ -8,7 +11,7 @@ public class RespRest<T>
 	private final RespHead head;
 	private final T body;
 
-	public RespRest(Resultado rtdo, T body)
+	private RespRest(Resultado rtdo, T body)
 	{
 		this.head = new RespHead( rtdo ); 
 		this.body = body;
@@ -26,5 +29,49 @@ public class RespRest<T>
     public String toString()
     {
         return ToStringUtils.toString(this);
+    }
+
+    public static <T> RespRest of(Resultado rtdo, T body)
+    {
+        return new RespRest<>(rtdo,body);
+    }
+    
+    public static <T> RespRest of(Respuesta<T> respBO)
+    {
+        if( respBO == null )
+        {
+            return new RespRest<>(null,null);
+        }
+        
+        return new RespRest<>(respBO.getResultado(), respBO.getContenido().orElse(null));
+    }
+    
+    public static <T> RespRest of(Resultado rtdo)
+    {
+        return new RespRest(rtdo,null);
+    }
+    
+    public static <T> RespRest of(T body)
+    {
+        return new RespRest(null, body);
+    }
+    	
+	public static <T> RespRest of(Class<?> clazz, Exception exception)
+	{
+    	Resultado rtdo = new ResultadoProceso();
+		rtdo.addException(clazz, exception);
+		return new RespRest<>(rtdo,null);
+	}
+
+  	public static <T> RespRest of(Mensaje mensaje)
+	{
+    	Resultado rtdo = new ResultadoProceso();
+		rtdo.addMensaje(mensaje);
+		return new RespRest<>(rtdo,null);
+	}
+
+    public static <T> RespRest exitosa()
+    {
+		return new RespRest(new ResultadoProceso(),null);
     }
 }
